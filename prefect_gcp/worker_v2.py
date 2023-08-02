058ba9ed06f5a90bd398eb7cf68a4b1792658013
Jack Perry <jack.e.perry@outlook.com>
2023-08-02 00:38:45 -0600

    👌 IMPROVE: more progress

--- prefect_gcp/worker_v2.py
+++ prefect_gcp/worker_v2.py
@@ -1,11 +1,25 @@
+import re
 import shlex
 from typing import TYPE_CHECKING, Any, Dict, Optional
 
+import anyio
+import googleapiclient
+from google.api_core.client_options import ClientOptions
+from googleapiclient import discovery
+from googleapiclient.discovery import Resource
+from prefect.logging.loggers import PrefectLogAdapter
+from prefect.utilities.asyncutils import run_sync_in_worker_thread
 from prefect.utilities.dockerutils import get_prefect_image_name
 from prefect.utilities.pydantic import JsonPatch
-from prefect.workers.base import BaseJobConfiguration
+from prefect.workers.base import (
+    BaseJobConfiguration,
+    BaseVariables,
+    BaseWorker,
+    BaseWorkerResult,
+)
 from pydantic import Field, validator
 
+from prefect_gcp.cloud_run_v2 import ExecutionV2, JobV2
 from prefect_gcp.credentials import GcpCredentials
 
 if TYPE_CHECKING:
@@ -237,3 +251,290 @@ class CloudRunV2WorkerJobConfiguration(BaseJobConfiguration):
                 f"{', '.join(incompatible)}"
             )
         return value
+
+
+class CloudRunV2WorkerVariables(BaseVariables):
+    """
+    Default variables for the Cloud Run v2 worker.
+
+    The schema for this class is used to populate the `variables` section of the
+    default base job template.
+    """
+
+    region: str = Field(
+        default="us-central1",
+        description="The region where the Cloud Run Job resides.",
+        example="us-central1",
+    )
+    credentials: Optional[GcpCredentials] = Field(
+        title="GCP Credentials",
+        default_factory=GcpCredentials,
+        description="The GCP Credentials used to initiate the "
+        "Cloud Run Job. If not provided credentials will be "
+        "inferred from the local environment.",
+    )
+    image: Optional[str] = Field(
+        default=None,
+        title="Image Name",
+        description=(
+            "The image to use for a new Cloud Run Job. "
+            "If not set, the latest Prefect image will be used. "
+            "See https://cloud.google.com/run/docs/deploying#images."
+        ),
+        example="docker.io/prefecthq/prefect:2-latest",
+    )
+    cpu: Optional[str] = Field(
+        default=None,
+        title="CPU",
+        description=(
+            "The amount of compute allocated to the Cloud Run Job. "
+            "(1000m = 1 CPU). See "
+            "https://cloud.google.com/run/docs/configuring/cpu#setting-jobs."
+        ),
+        example="1000m",
+        regex=r"^(\d*000)m$",
+    )
+    memory: Optional[str] = Field(
+        default=None,
+        title="Memory",
+        description=(
+            "The amount of memory allocated to the Cloud Run Job. "
+            "Must be specified in units of 'G', 'Gi', 'M', or 'Mi'. "
+            "See https://cloud.google.com/run/docs/configuring/memory-limits#setting."
+        ),
+        example="512Mi",
+        regex=r"^\d+(?:G|Gi|M|Mi)$",
+    )
+    vpc_connector_name: Optional[str] = Field(
+        default=None,
+        title="VPC Connector Name",
+        description="The name of the VPC connector to use for the Cloud Run Job.",
+    )
+    service_account: Optional[str] = Field(
+        default=None,
+        title="Service Account",
+        description="The name of the service account to use for the task execution "
+        "of Cloud Run Job. By default Cloud Run jobs run as the default "
+        "Compute Engine Service Account. ",
+        example="service-account@example.iam.gserviceaccount.com",
+    )
+    keep_job: Optional[bool] = Field(
+        default=False,
+        title="Keep Job After Completion",
+        description="Keep the completed Cloud Run Job after it has run.",
+    )
+    timeout: Optional[int] = Field(
+        default=600,
+        gt=0,
+        le=8400,
+        title="Job Timeout",
+        description=(
+            "The length of time that Prefect will wait for Cloud Run Job state changes."
+        ),
+    )
+
+
+class CloudRunV2WorkerResult(BaseWorkerResult):
+    """
+    Contains information about the final state of a completed process
+    """
+
+
+class CloudRunV2Worker(BaseWorker):
+    """
+    Prefect worker that executes flow runs within Cloud Run Jobs.
+    """
+
+    type = "cloud-run-v2"
+    job_configuration = CloudRunV2WorkerJobConfiguration
+    job_configuration_variables = CloudRunV2WorkerVariables
+    _description = (
+        "Execute flow runs within containers on Google Cloud Run v2. Requires "
+        "a Google Cloud Platform account."
+    )
+    _display_name = "Google Cloud Run v2"
+    _documentation_url = "https://prefecthq.github.io/prefect-gcp/worker_v2/"
+    _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/4SpnOBvMYkHp6z939MDKP6/549a91bc1ce9afd4fb12c68db7b68106/social-icon-google-cloud-1200-630.png?h=250"  # noqa
+
+    def _create_job_error(self, exc, configuration):
+        """Provides a nicer error for 404s when trying to create a Cloud Run Job."""
+        # TODO consider lookup table instead of the if/else,
+        # also check for documented errors
+        if exc.status_code == 404:
+            raise RuntimeError(
+                f"Failed to find resources at {exc.uri}. Confirm that region"
+                f" '{self.region}' is the correct region for your Cloud Run Job and"
+                f" that {configuration.project} is the correct GCP project. If"
+                f" your project ID is not correct, you are using a Credentials block"
+                f" with permissions for the wrong project."
+            ) from exc
+        raise exc
+
+    def _job_run_submission_error(self, exc, configuration):
+        """Provides a nicer error for 404s when submitting job runs."""
+        if exc.status_code == 404:
+            pat1 = r"The requested URL [^ ]+ was not found on this server"
+            # pat2 = (
+            #     r"Resource '[^ ]+' of kind 'JOB' in region '[\w\-0-9]+' "
+            #     r"in project '[\w\-0-9]+' does not exist"
+            # )
+            if re.findall(pat1, str(exc)):
+                raise RuntimeError(
+                    f"Failed to find resources at {exc.uri}. "
+                    f"Confirm that region '{self.region}' is "
+                    f"the correct region for your Cloud Run Job "
+                    f"and that '{configuration.project}' is the "
+                    f"correct GCP project. If your project ID is not "
+                    f"correct, you are using a Credentials "
+                    f"block with permissions for the wrong project."
+                ) from exc
+            else:
+                raise exc
+
+        raise exc
+
+    async def run(
+        self,
+        flow_run: "FlowRun",
+        configuration: CloudRunV2WorkerJobConfiguration,
+        task_status: Optional[anyio.abc.TaskStatus] = None,
+    ) -> CloudRunV2WorkerResult:
+        """
+        Executes a flow run within a Cloud Run Job and waits for the flow run
+        to complete.
+
+        Args:
+            flow_run: The flow run to execute
+            configuration: The configuration to use when executing the flow run.
+            task_status: The task status object for the current flow run. If provided,
+                the task will be marked as started.
+
+        Returns:
+            CloudRunWorkerResult: A result object containing information about the
+                final state of the flow run
+        """
+
+        logger = self.get_flow_run_logger(flow_run)
+
+        with self._get_client(configuration) as client:
+            await run_sync_in_worker_thread(
+                self._create_job_and_wait_for_registration,
+                configuration,
+                client,
+                logger,
+            )
+            job_execution = await run_sync_in_worker_thread(
+                self._begin_job_execution, configuration, client, logger
+            )
+
+            if task_status:
+                task_status.started(configuration.job_name)
+
+            result = await run_sync_in_worker_thread(
+                self._watch_job_execution_and_get_result,
+                configuration,
+                client,
+                job_execution,
+                logger,
+            )
+            return result
+
+    @staticmethod
+    def _get_client(configuration: CloudRunV2WorkerJobConfiguration) -> Resource:
+        """
+        Get the base client needed for interacting with GCP APIs.
+
+        Returns:
+            A client for interacting with GCP Cloud Run v2 API.
+        """
+        api_endpoint = f"https://{configuration.region}-run.googleapis.com"
+        gcp_creds = configuration.credentials.get_credentials_from_service_account()
+        options = ClientOptions(api_endpoint=api_endpoint)
+
+        return (
+            discovery.build(
+                "run",
+                "v2",
+                client_options=options,
+                credentials=gcp_creds,
+            )
+            .projects()
+            .locations()
+        )
+
+    def _create_job_and_wait_for_registration(
+        self,
+        configuration: CloudRunV2WorkerJobConfiguration,
+        client: Resource,
+        logger: PrefectLogAdapter,
+    ) -> None:
+        """
+        Create a new job wait for it to finish registering.
+        """
+        try:
+            logger.info(f"Creating Cloud Run Job {configuration.job_name}")
+
+            JobV2.create(
+                client=client,
+                project=configuration.credentials.project,
+                location=configuration.region,
+                body=configuration.job_body,
+            )
+        except googleapiclient.errors.HttpError as exc:
+            self._create_job_error(exc, configuration)
+
+        try:
+            self._wait_for_job_creation(
+                client=client, configuration=configuration, logger=logger
+            )
+        except Exception:
+            logger.exception(
+                "Encountered an exception while waiting for job run creation"
+            )
+            if not configuration.keep_job:
+                logger.info(
+                    f"Deleting Cloud Run Job {configuration.job_name} from "
+                    "Google Cloud Run."
+                )
+                try:
+                    JobV2.delete(
+                        client=client,
+                        project=configuration.credentials.project,
+                        location=configuration.region,
+                        job_name=configuration.job_name,
+                    )
+                except Exception:
+                    logger.exception(
+                        "Received an unexpected exception while attempting to delete"
+                        f" Cloud Run Job {configuration.job_name!r}"
+                    )
+            raise
+
+    def _begin_job_execution(
+        self,
+        configuration: CloudRunV2WorkerJobConfiguration,
+        client: Resource,
+        logger: PrefectLogAdapter,
+    ) -> ExecutionV2:
+        """
+        Submit a job run for execution and return the execution object.
+        """
+        try:
+            logger.info(
+                f"Submitting Cloud Run Job {configuration.job_name!r} for execution."
+            )
+            submission = JobV2.run(
+                client=client,
+                project=configuration.credentials.project,
+                location=configuration.region,
+                job_name=configuration.job_name,
+            )
+
+            job_execution = ExecutionV2.get(
+                client=client,
+                execution_name=submission["metadata"]["name"],
+            )
+
+            return job_execution
+        except Exception as exc:
+            self._job_run_submission_error(exc, configuration)
